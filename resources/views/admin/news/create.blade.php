5a1f3c8e65c1c67311f68226d791b0673b92b428
asahi <asahi38saya@yahoo.co.jp>
2019-08-01 11:29:29 +0000

    second commit

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -4,7 +4,7 @@
  <div class="container">
    <div class="row">
      <div class="col-md-8 mx-auto">
-       <h2>ニュース新規作成</h2>
+       <h2>新しいニュース作成</h2>
        <form action="{{ action('Admin\NewsController@create') }}" method="post" enctype="multipart/form-data">
         
          @if (count($errors) > 0)
